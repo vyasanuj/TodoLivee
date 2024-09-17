61a004d35026711d580100f6587bef6db6fbbd47
vyasanuj <vanuj616@gmail.com>
2024-09-17 18:58:52 +0530

    functionality Added

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,6 @@ function App() {
   return (
     <>
     <h1 className ="text-3xl font-bold text-white-100 text-center mb-4">Plan Your Day: Create and Manage Your Todos</h1>
-
-
-
     <AddTodo/>
     <Todos/>
     </>
